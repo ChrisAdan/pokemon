f68b2df04ad8766104d2dd6f9421e69721ad0cca
Chris Adan <55331613+ChrisAdan@users.noreply.github.com>
2025-03-24 19:04:11 -0400

    Add fetch_all_moves() to extract move list

--- scripts/fetch_pokemon_data.py
+++ scripts/fetch_pokemon_data.py
@@ -7,9 +7,9 @@ headers = {
     'Content-Type':'application/json',
 }
 
-def dispatch(response):
+def dispatch(response, type):
     if response.status_code == 200:
-        return response.json()
+        return response.json()['data'][type]
     else:
         raise Exception("API call failed with status code " + str(response.status_code))
 
@@ -24,7 +24,7 @@ def fetch_single_pokemon(name='dragonite'):
     }
 
     response = requests.post(url, json={'query':query, 'variables':variables}, headers=headers)
-    dispatch(response)
+    return dispatch(response, 'getPokemon')
 
 def fetch_all_pokemon(include_special=False):
     '''Fetch a list by calling getAllPokemon(). Return response.json().
@@ -36,7 +36,7 @@ def fetch_all_pokemon(include_special=False):
             'offset':89
         }
     response = requests.post(url, json={'query':query, 'variables':variables if variables else None}, headers=headers)
-    dispatch(response)
+    return dispatch(response, 'getAllPokemon')
 
 def fetch_single_move(name='absorb'):
     '''Pass string with Move name to call getMove(). Return response.json().
@@ -48,7 +48,7 @@ def fetch_single_move(name='absorb'):
         'move':name
     }
     response = requests.post(url, json={'query':query, 'variables':variables}, headers=headers)
-    dispatch(response)
+    return dispatch(response, 'getMove')
 
 def fetch_single_nature(name='jolly'):
     '''Pass string with Nature name to call getNature(). Return response.json().
@@ -60,7 +60,7 @@ def fetch_single_nature(name='jolly'):
         'nature':name
     }
     response = requests.post(url, json={'query':query, 'variables':variables}, headers=headers)
-    dispatch(response)
+    return dispatch(response, 'getNature')
 
 def fetch_all_natures():
     '''Fetch a list by calling getAllNatures(). Return response.json().'''
@@ -68,7 +68,7 @@ def fetch_all_natures():
         query = ''.join(line for line in file if not line.strip().startswith('#'))
 
     response = requests.post(url, json={'query':query}, headers=headers)
-    dispatch(response)
+    return dispatch(response, 'getAllNatures')
 
 def fetch_type_matchup(primary_type='fire', secondary_type='ghost'):
     '''Pass a string primary_type and secondary_type to call getTypeMatchup(). Return response.json().
@@ -82,6 +82,18 @@ def fetch_type_matchup(primary_type='fire', secondary_type='ghost'):
         'secondaryType': secondary_type.lower()
         }
         response = requests.post(url, json={'query': query, 'variables': variables}, headers=headers)
-        dispatch(response)
+    return dispatch(response, 'getTypeMatchup')
+
+def fetch_all_moves():
+    '''Fetch all available move names from the PokeAPI REST.'''
+
+    url = 'https://pokeapi.co/api/v2/move?limit=1000'
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+        return [move['name'] for move in data['results']]
+    else:
+        raise Exception('Failed to fetch move list')
 
 
