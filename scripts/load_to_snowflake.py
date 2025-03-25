c892391527331c1103b7c51bccf044550c1f3a56
Chris Adan <55331613+ChrisAdan@users.noreply.github.com>
2025-03-25 19:35:18 -0400

    Test new string replacement

--- scripts/load_to_snowflake.py
+++ scripts/load_to_snowflake.py
@@ -46,7 +46,8 @@ def load_to_snowflake(data, schema):
         raise ValueError(f"Unexpected data format in {data}")
 
     record_id = hash(data['key']) % (10 ** 20)
-    name = data.get('key', 'Unknown').replace('-','_')
+    name = data.get('key', 'Unknown')
+    name = name.replace('-', '_')
     created_at = datetime.datetime.now(datetime.timezone.utc)
     raw_response = json.dumps(data, ensure_ascii = False)
 
