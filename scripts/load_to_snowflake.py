a4b760ec5cb64938538a5d8e0669136e0a9133d5
Chris Adan <55331613+ChrisAdan@users.noreply.github.com>
2025-03-24 20:24:54 -0400

    Cleanup + standardize environment variable naming convention

--- scripts/load_to_snowflake.py
+++ scripts/load_to_snowflake.py
@@ -12,14 +12,14 @@ tables = {
 
 load_dotenv()
 
-def connect_to_snowflake(schema='RAW'):
+def connect_to_snowflake():
     conn = snowflake.connector.connect(
         user=os.getenv('SNOWFLAKE_USER'),
         password=os.getenv('SNOWFLAKE_PASSWORD'),
         account=os.getenv('SNOWFLAKE_ACCOUNT'),
-        warehouse=os.getenv('POKEMON_WH'),
-        database=os.getenv('POKEMON_DBNAME'),
-        schema=schema
+        warehouse=os.getenv('SNOWFLAKE_WAREHOUSE'),
+        database=os.getenv('SNOWFLAKE_DATABASE'),
+        schema=os.getenv('SNOWFLAKE_SCHEMA')
     )
 
     cursor = conn.cursor()
