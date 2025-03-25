bd2a1a57c412ce5f4f54ddf010e61da3f3fa244f
Chris Adan <55331613+ChrisAdan@users.noreply.github.com>
2025-03-24 20:01:14 -0400

    Add single Type matchup transformation

---
+++ scripts/transform.py
@@ -0,0 +1,26 @@
+def transform_matchup_data(raw_data):
+    '''Function to unpack Type matchup API response. Unpacks attacking and defending
+    properties and nested effectiveness data.
+    Arguments:
+        raw_data: API response dict for single Type matchup
+    Returns: transformed_data objects'''
+      
+    attacking = raw_data['attacking']
+    defending = raw_data['defending']
+    
+    transformed_data = {
+        'attacking_double_effective': attacking['doubleEffectiveTypes'],
+        'attacking_double_resisted': attacking['doubleResistedTypes'],
+        'attacking_effective': attacking['effectiveTypes'],
+        'attacking_effectless': attacking['effectlessTypes'],
+        'attacking_normal': attacking['normalTypes'],
+        'attacking_resisted': attacking['resistedTypes'],
+        
+        'defending_double_effective': defending['doubleEffectiveTypes'],
+        'defending_double_resisted': defending['doubleResistedTypes'],
+        'defending_effective': defending['effectiveTypes'],
+        'defending_effectless': defending['effectlessTypes'],
+        'defending_normal': defending['normalTypes'],
+        'defending_resisted': defending['resistedTypes'],
+    }
+    return transformed_data
