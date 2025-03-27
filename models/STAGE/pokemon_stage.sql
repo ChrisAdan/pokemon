ba6f4dba4f96d4b5e639fa4948a427ffe59c9770
Chris Adan <55331613+ChrisAdan@users.noreply.github.com>
2025-03-26 20:19:21 -0400

    Add pokemon_db.stage.pokemon_stage

--- models/STAGE/pokemon_stage.sql
+++ models/STAGE/pokemon_stage.sql
@@ -6,38 +6,39 @@
 select
     -- Basic descriptive information
     id,
-    raw_response:num::int as number comment 'The Pokemon''s local PokeDex number (duplicates between generations)',
+    raw_response:num::int as number,
     name,
+    raw_response:types[0].name::varchar(100) as primary_type,
+    raw_response:types[1].name::varchar(100) as secondary_type,
     -- Base stats
-    raw_response:baseStatsTotal::int as base_stats_total comment 'Sum of Pokemon Base Stats',
-    raw_response:baseStats.hp::int as hp comment 'Base HP',
-    raw_response:baseStats.attack::int as attack comment 'Base Attack stat',
-    raw_response:baseStats.specialattack::int as special_attack comment 'Base Sp. Attack stat',
-    raw_response:baseStats.defense::int as defense comment 'Base Defense stat',
-    raw_response:baseStats.specialdefense::int as specialdefense comment 'Base Sp. Defense stat',
-    raw_response:baseStats.speed::int as speed comment 'Base Speed stat',
+    raw_response:baseStatsTotal::int as base_stats_total,
+    raw_response:baseStats.hp::int as hp,
+    raw_response:baseStats.attack::int as attack,
+    raw_response:baseStats.specialattack::int as special_attack,
+    raw_response:baseStats.defense::int as defense,
+    raw_response:baseStats.specialdefense::int as specialdefense,
+    raw_response:baseStats.speed::int as speed,
     -- Gender distribution
-    raw_response:gender.male::number(5, 2) as percent_male comment 'Percentage of Pokemon that are Male',
-    raw_response:gender.female::number(5, 2) as percent_female comment 'Percentage of Pokemon that are Female',
+    {{ convert_percent_string_to_number('raw_response:gender.male') }} as percent_male,
+    {{ convert_percent_string_to_number('raw_response:gender.female') }} as percent_female,
     -- Physical characteristics
-    raw_response:height::number(5, 2) as height comment 'Pokemon Height in meters',
-    raw_response:weight::number(5, 2) as weight comment 'Pokemon Weight in meters',
-    raw_response:evolutionLevel::int as evolution_level comment 'Pokemon''s evolution level, if applicable',
-    raw_response:levellingRate::varchar(255) as leveling_rate comment 'The leveling rate of the Pokemon',
+    raw_response:height::number(5, 2) as height,
+    raw_response:weight::number(5, 2) as weight,
+    raw_response:levellingRate::varchar(255) as leveling_rate,
     -- Egg context
-    raw_response:isEggObtainable::boolean as is_egg_obtainable comment 'Is Pokemon obtainable from egg',
-    raw_response:minimumHatchTime::int as min_hatch_time comment 'The minimum egg hatch time',
-    raw_response:maximumHatchTime::int as max_hatch_time comment 'The maximum egg hatch time',
+    raw_response:isEggObtainable::boolean as is_egg_obtainable,
+    raw_response:minimumHatchTime::int as min_hatch_time,
+    raw_response:maximumHatchTime::int as max_hatch_time,
     -- Special status
-    raw_response:legendary::boolean as is_legendary comment 'Is Pokemon legendary',
-    raw_response:mythical::boolean as is_mythical comment 'Is Pokemon mythical',
+    raw_response:legendary::boolean as is_legendary,
+    raw_response:mythical::boolean as is_mythical,
     -- EV yields
-    raw_response:evYields.hp::int as ev_yield_hp comment 'HP EV Yield',
-    raw_response:evYields.attack::int as ev_yield_attack comment 'Attack EV Yield',
-    raw_response:evYields.specialattack::int as ev_yield_sp_attack comment 'Sp. Attack EV Yield',
-    raw_response:evYields.defense::int as ev_yield_defense comment 'Defense EV Yield',
-    raw_response:evYields.specialdefense::int as ev_yield_sp_def comment 'Sp. Defense EV Yield',
-    raw_response:evYields.speed::int as ev_yield_speed comment 'Speed EV Yield'
-from {{ ref('pokemon_raw') }}
+    raw_response:evYields.hp::int as ev_yield_hp,
+    raw_response:evYields.attack::int as ev_yield_attack,
+    raw_response:evYields.specialattack::int as ev_yield_sp_attack,
+    raw_response:evYields.defense::int as ev_yield_defense,
+    raw_response:evYields.specialdefense::int as ev_yield_sp_def,
+    raw_response:evYields.speed::int as ev_yield_speed
+from pokemon_db.raw.pokemon_raw
 
 
