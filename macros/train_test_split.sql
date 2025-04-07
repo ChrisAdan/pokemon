65e9bded308bb0f5fa33e3bfe889edc75146524f
Chris Adan <55331613+ChrisAdan@users.noreply.github.com>
2025-04-07 19:07:16 -0400

    Add train_test_split macro for data preparation

--- macros/train_test_split.sql
+++ macros/train_test_split.sql
@@ -1,13 +1,24 @@
-{% macro train_test_split(source_relation, target_column, partitions = 5, split_ratio=0.8) %}
-with base as (
+{% macro train_test_split(features_cte, targets_cte, partitions=5, split_ratio=0.8) %}
+with features as (
+    {{ features_cte }}
+),
+targets as (
+    {{ targets_cte }}
+),
+joined as (
+    select f.*, t.*
+    from features f
+    join targets t on f.id = t.id
+),
+base as (
     select *, 
-           ntile({{ partitions|int }}) over (partition by {{ target_column }} order by random()) as split_bucket
-    from {{ source_relation }}
+           ntile({{ partitions|int }}) over (partition by t.{{ target_column }} order by random()) as split_bucket
+    from joined
 ),
 splits as (
     select *, 
            case 
-               when split_bucket <= {{ (split_ratio * partitions) | round(0, 'floor') | int }} then 'train'
+               when split_bucket <= {{ (split_ratio * partitions)|round(0, 'floor')|int }} then 'train'
                else 'test'
            end as split
     from base
